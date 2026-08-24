cb685877d316532623e25c8bb16ee3753db4e624
crashtestdummy01 <testdummyt01@gmail.com>
2026-08-24 16:53:23 +0200

    Update force logger

--- experiments/force_logger.py
+++ experiments/force_logger.py
@@ -1,30 +1,67 @@
+#!/usr/bin/env python3
+
+import csv
 import rclpy
+import numpy as np
+
+
+from enum import Enum, auto
+from collections import deque
+from string import Template
 from rclpy.node import Node
 from geometry_msgs.msg import WrenchStamped
-import math
-import csv
 
-class ForceListener(Node):
-    def __init__(self):
-        super().__init__('force_listener')
 
-        self.subscription = self.create_subscription(
+CONTACT_THRESH_N = 6.5
+CSV_FILE = Template("exp-${seq_number}")
+
+
+class LoggerState(Enum):
+    READY = auto()
+    REC = auto()
+
+class ForceLogger(Node):
+
+    def __init__(self, filename):
+        super().__init__("force_logger")
+        
+        self.force_sub = self.create_subscription(
             WrenchStamped,
             '/cartesian_impedance_controller/measured_force',
             self.wrench_callback,
             10
         )
 
-        self.latest_msg = None
+        self.ring_buffer = deque(maxlen=100)
 
-        self.display_timer = self.create_timer(0.2, self.display_callback)
-
-        self.csv_file = open('force_data.csv', 'w', newline='')
-        self.csv_writer = csv.writer(self.csv_file)
-        self.csv_writer.writerow(['time_s', 'force_x', 'force_y', 'force_z', 'torque_x', 'torque_y', 'torque_z'])
+        self.state = LoggerState.READY
         self.start_time = None
+        self.time_since_start = 0
 
-    def wrench_callback(self, msg: WrenchStamped):
+        self.csv_file = open(f"{filename}.csv", 'w', newline='')
+        self.csv_writer = csv.writer(self.csv_file)
+
+        self.csv_writer.writerow(['time_s', 'force_x', 'force_y', 'force_z', 'torque_x', 'torque_y', 'torque_z'])
+
+        self.get_logger().info(f"Logger READY -> '{filename}' | Trigger threshold: {CONTACT_THRESH_N} N")
+
+    def wrench_callback(self, msg):
+        force_z = abs(msg.wrench.force.z)
+
+        if self.state is LoggerState.READY:
+            self.ring_buffer.append(msg)
+
+            if force_z >= CONTACT_THRESH_N:
+                self.state = LoggerState.REC
+                self.get_logger().info(f"Contact hit ({force_z:.2f} N)! Flushing buffer and recording.")
+
+                if len(self.ring_buffer) > 0:
+                    for buf_msg in self.ring_buffer:
+                        self.write_row(buf_msg)
+        else:
+            self.write_row(msg)
+
+    def write_row(self, msg):
         self.latest_msg = msg
 
         stamp = msg.header.stamp
@@ -43,30 +80,19 @@ class ForceListener(Node):
             torque.x, torque.y, torque.z
         ])
 
-    def display_callback(self):
-        if self.latest_msg is None:
-            return
-
-        force = self.latest_msg.wrench.force
-        torque = self.latest_msg.wrench.torque
-
-        f_mag = math.sqrt(force.x**2 + force.y**2 + force.z**2)
-        t_mag = math.sqrt(torque.x**2 + torque.y**2 + torque.z**2)
-
-        self.get_logger().info(
-            f"\n--- Measured Wrench (5 Hz Display) ---\n"
-            f"Force  [N] : X={force.x:7.2f} | Y={force.y:7.2f} | Z={force.z:7.2f} | Mag={f_mag:7.2f}\n"
-            f"Torque [Nm]: X={torque.x:7.2f} | Y={torque.y:7.2f} | Z={torque.z:7.2f} | Mag={t_mag:7.2f}"
-        )
-
     def destroy_node(self):
         if hasattr(self, 'csv_file') and not self.csv_file.closed:
             self.csv_file.close()
+            self.get_logger().info("Recording done.")
+
         super().destroy_node()
 
+
 def main(args=None):
+    exp_num = sys.argv[1] if len(sys.argv) > 1 else "0"
+
     rclpy.init(args=args)
-    node = ForceListener()
+    node = ForceLogger(f"force_data-{exp_num}")
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
@@ -76,5 +102,5 @@ def main(args=None):
         if rclpy.ok():
             rclpy.shutdown()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
